eab5fcee4ed158f2e5f8d9f5d5b36394a5d800c8
riizdo <ricardoizquierdopro@gmail.com>
2019-04-05 00:41:34 +0200

    trabajando en clase player

--- clase_player.py
+++ clase_player.py
@@ -4,3 +4,19 @@ class Player:
     def __init__(self, numero):
         self.__numero = numero
         self.__cuerpo = turtle.Turtle()
+        
+    def __str__(self):
+        return "Player {}".format(self._numero)
+    
+    def __comp(self, valor):
+        try:
+            return int(valor)
+        except:
+            return None
+        
+    def forma(self, valor = None):
+        if valor == None:
+            return self.__cuerpo.shape()
+        elif valor == 'arrow' or valor == 'turtle' or valor == 'circle' or valor == 'square' or valor == 'triangle' or valor == 'classic':
+            self.__cuerpo.shape(valor)
+    
