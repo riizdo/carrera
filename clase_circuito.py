bf5a1bf2e12020630ff8af80ff93fd65583d89dc
riizdo <ricardoizquierdopro@gmail.com>
2019-04-04 20:40:07 +0200

    desarrollando clase circuito

--- clase_circuito.py
+++ clase_circuito.py
@@ -4,7 +4,30 @@ class Circuito():
     corredores = []
     
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         
         self.__screen = turtle.Screen()
+        
+    def __str__(self):
+        return 'pantalla del circuito'
+    
+    def __comp(self, valor):
+        try:
+            return int(valor)
+        except:
+            return None
+    
+    def width(self, width = None):
+        width = self.__comp(width)
+        if width == None:
+            return self.__width
+        else:
+            self.__width = width
+            
+    def height(self, height = None):
+        height = self.__comp(height)
+        if height == None:
+            return self.__height
+        else:
+            self.__height = height
