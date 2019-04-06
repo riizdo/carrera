920d2484439a7c6225df6350e2a8d1eea2eb07ec
riizdo <ricardoizquierdopro@gmail.com>
2019-04-07 00:14:20 +0200

    clases circuito y player

--- clase_circuito.py
+++ clase_circuito.py
@@ -1,18 +1,20 @@
 import turtle
 import clase_player
+import random
 
 class Circuito():
     
     
     def __init__(self, width = 900, height = 500):
         self.__corredor = []
+        self.__width = width
+        self.__height = height
         
         self.__screen = turtle.Screen()
         self.__screen.setup(width, height)
+        self.__screen.setworldcoordinates(0, height, width, 0)
         
-        self.player(len(self.__corredor), True)
-        self.__corredor[0].nombre('pepe')
-        self.__corredor[0].color('green')
+        self.player(True)
         
     def __str__(self):
         return 'pantalla del circuito'
@@ -22,26 +24,52 @@ class Circuito():
             return int(valor)
         except:
             return None
-    
-    def width(self, width = None):
-        width = self.__comp(width)
-        if width == None:
-            return self.__width
-        else:
-            self.__width = width
+        
+    def player(self, nuevo = False): 
+        if nuevo == False:
+            return self.__corredor
+        elif nuevo == True:
+            indice = len(self.__corredor)
+            nombre = self.__screen.textinput('Nombre', 'Introduzca el nombre')
+            color = self.__screen.textinput('color', 'Introduzca el color')
+            self.__corredor.append(clase_player.Player(nombre))
+            self.__corredor[indice].penup()
+            self.__corredor[indice].color(color)
+            self.__corredor[indice].forward(5)
+            self.__corredor[indice].left(90)
+            self.__corredor[indice].forward(30)
             
-    def height(self, height = None):
-        height = self.__comp(height)
-        if height == None:
-            return self.__height
-        else:
-            self.__height = height
+            for indice in range(len(self.__corredor)):
+                self.__corredor[indice].forward(30)
             
-    def player(self, numero, nuevo = False):
-        if nuevo == False and numero < len(self.__corredor):
-            return self.__corredor[numero]
-        elif nuevo == True and numero == len(self.__corredor):
-            self.__corredor.append(clase_player.Player(numero, 'pepe'))
-            
-    def cantidad(self):
-        return len(self.__corredor)
+    def ejecucion(self):
+        iniciando = True
+        while iniciando:
+            instruccion = self.__screen.textinput('Instruccion', 'player: añade un player\ninicio: inicia la carrera')
+            if instruccion == 'player':
+                self.player(True)
+            elif instruccion == 'inicio':
+                iniciando = False
+                
+        for indice in range(len(self.__corredor)):
+            self.__corredor[indice].left(270)
+                    
+        juego = True                        
+        while juego:
+            finish = self.__width - 30
+            for indice in range(len(self.__corredor)):
+                avance = random.randint(1, 6)
+                self.__corredor[indice].forward(avance)
+                
+                if self.__corredor[indice].position()[0] >= finish:
+                    juego = False
+                    self.__corredor[indice].left(90)
+                    self.__corredor[indice].sety(self.__height / 2)
+                    self.__corredor[indice].left(90)
+                    self.__corredor[indice].setx(self.__width / 2)
+                    self.__corredor[indice].write(self.__corredor[indice].position()[0])
+                
+                
+if __name__ == '__main__':
+    circuito = Circuito()
+    circuito.ejecucion()
--- clase_player.py
+++ clase_player.py
@@ -1,42 +1,18 @@
 import turtle
 
-class Player:
-    def __init__(self, numero, nombre):
-        self.__numero = numero
+class Player(turtle.Turtle):
+    
+    def __init__(self, nombre):
+        turtle.Turtle.__init__(self, 'turtle', 1000, True)
         self.__nombre = nombre
-        self.__cuerpo = turtle.Turtle('turtle')
         
     def __str__(self):
-        return "Player {}, corredor {}".format(self.__nombre, self.__numero)
+        return "player {}". format(self.__nombre)
     
-    def __comp(self, valor):
-        try:
-            return int(valor)
-        except:
-            return None
-        
-    def forma(self, valor = None):
-        if valor == None:
-            return self.__cuerpo.shape()
-        elif valor == 'arrow' or valor == 'turtle' or valor == 'circle' or valor == 'square' or valor == 'triangle' or valor == 'classic':
-            self.__cuerpo.shape(valor)
-    
-    def numero(self, valor = None):
-        valor = comp(valor)
-        if valor == None:
-            return self.__numero
-        else:
-            self.__numero = numero
-        
     def nombre(self, nombre = None):
         if nombre == None:
             return self.__nombre
         else:
             self.__nombre = nombre
             
-    def color(self, color = None):
-        if color == None:
-            return self.__cuerpo.color()
-        else:
-            self.__cuerpo.color(color)
-            
+    
